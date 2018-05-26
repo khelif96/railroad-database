576fb20e102080a838bff286cfd91e4e6330cbef
khelif96 <khelif96@gmail.com>
2018-05-25 22:59:15 -0400

    Created route to get passenger info by api_key

--- railroad-backend/Routes/api.js
+++ railroad-backend/Routes/api.js
@@ -6,14 +6,16 @@ const router = express.Router();
 
 const apiHome = require('../Controller/apiHome');
 const stations = require('../Controller/stations');
-const passengers = require('../Controller/auth/passenger')
+const authpassengers = require('../Controller/auth/passenger')
+const passengers = require('../Controller/passenger')
 router.get('/', apiHome.getApi);
 
 router.get('/stations', stations.getStations);
 
-router.post('/auth/registerPassenger',passengers.registerPassenger);
-router.post('/auth/loginPassenger',passengers.loginPassenger);
+router.post('/auth/registerPassenger',authpassengers.registerPassenger);
+router.post('/auth/loginPassenger',authpassengers.loginPassenger);
 
+router.post('/passengers/api_key', passengers.getPassengerByApikey);
 
 router.use(apiHome.invalidPath);
 module.exports = router;

3efcd0e1e123b597236e040333cccfa24c30bb46
Satyam <sharma.satyam@outlook.com>
2018-05-27 15:07:10 -0400

    getTrains funtion: just a wrapper for select * from trains

---
+++ railroad-backend/Controller/trains.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6 */
+var con = require('../Utils/db');
+
+exports.getTrains = (req, res) => {
+  con.query('select * from trains',function(error,results,fields){
+  if(error) res.status(500).json({error: "Database error"});
+  res.json(results);
+  })
+  
+};

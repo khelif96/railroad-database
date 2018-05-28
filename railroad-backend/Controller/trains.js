f80722e560b7e8dd5ecc769bad1a830766cc3944
Satyam <sharma.satyam@outlook.com>
2018-05-28 15:16:10 -0400

    ; and tested

--- railroad-backend/Controller/trains.js
+++ railroad-backend/Controller/trains.js
@@ -2,7 +2,7 @@
 var con = require('../Utils/db');
 
 exports.getTrains = (req, res) => {
-  con.query('select * from trains',function(error,results,fields){
+  con.query('select * from trains;',function(error,results,fields){
   if(error) res.status(500).json({error: "Database error"});
   res.json(results);
   })

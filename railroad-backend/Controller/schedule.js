dc728b25b38811446a80b52a44405a61ea6cfd62
khelif96 <khelif96@gmail.com>
2018-05-28 23:57:20 -0400

    Finished Schedule page

--- railroad-backend/Controller/schedule.js
+++ railroad-backend/Controller/schedule.js
@@ -2,8 +2,23 @@
 var con = require('../Utils/db');
 
 exports.getSchedule = (req,res) => {
-  var getStopsAt = 'select * from stops_at limit 50';
+  var getStopsAt = 'select * from stops_at;';
   con.query(getStopsAt, function(error,results){
-    res.json(results)
+    if(error) res.status(500).json({error})
+    console.log("Get over here");
+    var response = [];
+    for(var i = 0; i<28; i++){
+      response.push([])
+    }
+    console.log(response.length)
+    console.log(response[0]);
+    for(var i = 0; i <results.length; i++){
+      try{
+      response[results[i].train_id-1].push(results[i])
+    }catch(error){
+      console.log("ERROR " +i + " "  + error)
+    }
+    }
+    res.json(response)
   })
 }
